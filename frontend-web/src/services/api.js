9226f59b628e5a7f322c2a9ecc444ff8a53f4c35
Sai Charan <SaiCharan@gmail.com>
2026-02-10 17:25:24 +0530

    ap

--- frontend-web/src/services/api.js
+++ frontend-web/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000/api' });
+const API_URL = 'https://chemicalvisualizer-4c97.onrender.com/api';
+
+const API = axios.create({ baseURL: API_URL});
 
 export const uploadFile = (formData) => API.post('/upload/', formData, {
     headers: { 'Content-Type': 'multipart/form-data' }
