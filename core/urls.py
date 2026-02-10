1c46b48318971b41a10ad973b196fe5a4001d532
Sai Charan <SaiCharan@gmail.com>
2026-02-10 17:17:32 +0530

    cv

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import UploadCSVView, DashboardDataView, HistoryView ,ExportPDFView,home
+from .views import UploadCSVView, DashboardDataView, HistoryView ,ExportPDFView
 
 urlpatterns = [
     path('upload/', UploadCSVView.as_view(), name='upload-csv'),
     path('summary/', DashboardDataView.as_view(), name='dashboard-summary'),
     path('history/', HistoryView.as_view(), name='upload-history'),
     path('export-pdf/', ExportPDFView.as_view(), name='export-pdf'),
-    path('', home),
 ]
